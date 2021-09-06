475c8fc7b0b39f220907a00a8d84fa239f782e48
DS-Dalton <daltondmason@live.com>
2021-09-06 14:40:38 -0400

    Started course project. Downloaded data, began R script for general analysis.

---
+++ Storm data analysis.R
@@ -0,0 +1,5 @@
+# Questions to be answered:
+  # Which types of events (EVTYPE) are most harmful to population health?
+  # Which EVTYPEs cause the most economic damage?
+# example analysis from RDPeng found here: https://www.rpubs.com/rdpeng/13396
+df <- read.csv('.\\data\\repdata_data_StormData.csv.bz2')
